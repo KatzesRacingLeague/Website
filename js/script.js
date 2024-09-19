2181b5044b59a084da28acdfe5b86bbe0038b985
Dario Iannello <dario.iannello1@gmail.com>
2024-09-19 20:01:50 +0200

    Finished Version 02.00.00

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 // Sanftes Scrollen zu Ankern auf der Seite
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
-  // Dynamische Anpassung der Hintergrundgröße
-  function adjustBackgroundSize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    document.body.style.backgroundSize = `${width}px ${height}px`;
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-    const dropdowns = document.querySelectorAll('.dropdown');
-  
-    menuToggle.addEventListener('click', function() {
-      navLinks.classList.toggle('active');
-    });
-  
-    dropdowns.forEach(dropdown => {
-      dropdown.addEventListener('click', function() {
-        this.querySelector('.dropdown-content').classList.toggle('show');
-      });
+});
+
+// Dynamische Anpassung der Hintergrundgröße
+function adjustBackgroundSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  document.body.style.backgroundSize = `${width}px ${height}px`;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+  const dropdowns = document.querySelectorAll('.dropdown');
+
+  menuToggle.addEventListener('click', function () {
+    navLinks.classList.toggle('active');
+  });
+
+  dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('click', function () {
+      this.querySelector('.dropdown-content').classList.toggle('show');
     });
   });
+});
