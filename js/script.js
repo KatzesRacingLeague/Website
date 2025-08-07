9a44363d2c6a9866c850bbbb6c309a0ceea79a44
Dario Iannello <dario.iannello1@gmail.com>
2025-08-07 21:43:05 +0200

    added races 3&4

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ allTextItems.forEach((text, index) => {
   div.classList.add('text-slide');
   div.textContent = text;
 
-  if (index < 3) {
+  if (index < 4) {
     div.style.color = 'var(--sundaysplit)';
   }
 
