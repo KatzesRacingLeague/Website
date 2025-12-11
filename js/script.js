f9baaa9ed353feb991d7dac498745e57ed3daef4
Dario Iannello <dario.iannello1@gmail.com>
2025-12-11 21:57:38 +0100

    imola

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ allTextItems.forEach((text, index) => {
   div.classList.add('text-slide');
   div.textContent = text;
 
-  if (index < 13) {
+  if (index < 15) {
     div.style.color = 'var(--sundaysplit)';
   }
 
